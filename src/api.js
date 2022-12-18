b0778d5dee2838cc134900ee666aaad0272c349a
Vladimir Kartyshov <Kratos@beelinerouter.net>
2022-12-18 20:49:03 +0300

    43-54

--- src/api.js
+++ src/api.js
@@ -1,6 +1,15 @@
-const API_KEY = '62f8a8af9dfc13f1b11d24d7afbe6c1449eb2ca41e025d20be437a754f4b8480'
+const API_KEY =
+  '62f8a8af9dfc13f1b11d24d7afbe6c1449eb2ca41e025d20be437a754f4b8480'
 
 export const loadTickers = (tickers) =>
   fetch(
-    `https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${tickers.join(',')}&api_key=${API_KEY}`
-  ).then((r) => r.json())
+    `https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${tickers.join(
+      ','
+    )}&api_key=${API_KEY}`
+  )
+    .then((r) => r.json())
+    .then((rawData) =>
+      Object.fromEntries(
+        Object.entries(rawData).map(([key, value]) => [key, 1 / value])
+      )
+    )
